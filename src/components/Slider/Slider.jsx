73f255031b7d1a96f2efb07434254816bc8fd15f
Islam <khasievislamcloud@gmail.com>
2023-03-23 14:42:35 +0300

    вывод главной страницы мап категорий и фильмов по категориям

--- src/components/Slider/Slider.jsx
+++ src/components/Slider/Slider.jsx
@@ -1,119 +1,33 @@
 import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchMovies } from "../../redux/features/movie/movieSlice";
 import styles from "./Slider.module.scss";
 
-const Slider = () => {
+const Slider = ({ catId }) => {
+  const dispatch = useDispatch();
+  const movies = useSelector((state) => state.movie.movies);
+  React.useEffect(() => {
+    dispatch(fetchMovies());
+  }, [dispatch]);
   return (
     <div className={styles.slider}>
-      <div className={styles.cardblock}>
-        <img
-          className={styles.card_img}
-          src='https://mf-static-ssl.more.tv/jackal/4072353/bb760c5f-765b-47a4-89aa-23759ca41891_W500_H710.avif'
-          alt=''
-        />
-        <div className={styles.card_text}>
-          <div className={styles.card_title}>Киборг</div>
-          <div className={styles.card_sub}>Подписка</div>
-        </div>
-      </div>
-      <div className={styles.cardblock}>
-        <img
-          className={styles.card_img}
-          src='https://mf-static-ssl.more.tv/jackal/2579173/85d4451c-5cbd-4dba-b1cd-2b0a480fc2ae_W500_H710.webp'
-          alt=''
-        />
-        <div className={styles.card_text}>
-          <div className={styles.card_title}>Киборг</div>
-          <div className={styles.card_sub}>Подписка</div>
-        </div>
-      </div>
-      <div className={styles.cardblock}>
-        <img
-          className={styles.card_img}
-          src='https://mf-static-ssl.more.tv/jackal/2579777/48afe766-e057-41f8-8be5-a8aca8e89753_W500_H710.webp'
-          alt=''
-        />
-        <div className={styles.card_text}>
-          <div className={styles.card_title}>Бронсон</div>
-          <div className={styles.card_sub}>Подписка</div>
-        </div>
-      </div>
-      <div className={styles.cardblock}>
-        <img
-          className={styles.card_img}
-          src='https://mf-static-ssl.more.tv/jackal/2586885/1cbd15b5-02cf-47af-a747-a651e1d3d288_W500_H710.webp'
-          alt=''
-        />
-        <div className={styles.card_text}>
-          <div className={styles.card_title}>Хоббит</div>
-          <div className={styles.card_sub}>Подписка</div>
-        </div>
-      </div>
-      <div className={styles.cardblock}>
-        <img
-          className={styles.card_img}
-          src='https://mf-static-ssl.more.tv/jackal/4102581/9b5d9350-2e8b-4266-837a-6c8e89c83cdd_W500_H710.avif'
-          alt=''
-        />
-        <div className={styles.card_text}>
-          <div className={styles.card_title}>Молчание ягнят</div>
-          <div className={styles.card_sub}>Подписка</div>
-        </div>
-      </div>
-      <div className={styles.cardblock}>
-        <img
-          className={styles.card_img}
-          src='https://mf-static-ssl.more.tv/jackal/4072353/bb760c5f-765b-47a4-89aa-23759ca41891_W500_H710.avif'
-          alt=''
-        />
-        <div className={styles.card_text}>
-          <div className={styles.card_title}>Киборг</div>
-          <div className={styles.card_sub}>Подписка</div>
-        </div>
-      </div>
-      <div className={styles.cardblock}>
-        <img
-          className={styles.card_img}
-          src='https://mf-static-ssl.more.tv/jackal/2579173/85d4451c-5cbd-4dba-b1cd-2b0a480fc2ae_W500_H710.webp'
-          alt=''
-        />
-        <div className={styles.card_text}>
-          <div className={styles.card_title}>Киборг</div>
-          <div className={styles.card_sub}>Подписка</div>
-        </div>
-      </div>
-      <div className={styles.cardblock}>
-        <img
-          className={styles.card_img}
-          src='https://mf-static-ssl.more.tv/jackal/2579777/48afe766-e057-41f8-8be5-a8aca8e89753_W500_H710.webp'
-          alt=''
-        />
-        <div className={styles.card_text}>
-          <div className={styles.card_title}>Бронсон</div>
-          <div className={styles.card_sub}>Подписка</div>
-        </div>
-      </div>
-      <div className={styles.cardblock}>
-        <img
-          className={styles.card_img}
-          src='https://mf-static-ssl.more.tv/jackal/2586885/1cbd15b5-02cf-47af-a747-a651e1d3d288_W500_H710.webp'
-          alt=''
-        />
-        <div className={styles.card_text}>
-          <div className={styles.card_title}>Хоббит</div>
-          <div className={styles.card_sub}>Подписка</div>
-        </div>
-      </div>
-      <div className={styles.cardblock}>
-        <img
-          className={styles.card_img}
-          src='https://mf-static-ssl.more.tv/jackal/4102581/9b5d9350-2e8b-4266-837a-6c8e89c83cdd_W500_H710.avif'
-          alt=''
-        />
-        <div className={styles.card_text}>
-          <div className={styles.card_title}>Молчание ягнят</div>
-          <div className={styles.card_sub}>Подписка</div>
-        </div>
-      </div>
+      {movies.map((movie) => {
+        if (movie.category === catId) {
+          return (
+            <div className={styles.cardblock}>
+              <img
+                className={styles.card_img}
+                src='https://mf-static-ssl.more.tv/jackal/4072353/bb760c5f-765b-47a4-89aa-23759ca41891_W500_H710.avif'
+                alt=''
+              />
+              <div className={styles.card_text}>
+                <div className={styles.card_title}>{movie.name}</div>
+                <div className={styles.card_sub}>{movie.sub}</div>
+              </div>
+            </div>
+          );
+        }
+      })}
     </div>
   );
 };
